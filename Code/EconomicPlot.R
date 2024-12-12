f21dddf4a5e8f1b2613360d1739e3e007debbb06
QasimA20 <qia5020@psu.edu>
2024-12-12 03:47:22 -0500

    Adding more code files

---
+++ Code/EconomicPlot.R
@@ -0,0 +1,16 @@
+library(ggplot2)
+
+data_climate_impact <- read.csv("climate_change_impact_on_agriculture_2024.csv") 
+
+#Create the scatter plot
+ggplot(data_climate_impact, aes(x = Extreme_Weather_Events, 
+                                y = Economic_Impact_Million_USD, 
+                                color = Crop_Yield_MT_per_HA)) +
+  geom_point(alpha = 0.7, size = 3) +
+  scale_color_viridis_c(name = "Crop Yield (MT/HA)") +
+  labs(
+    title = "Economic Impact vs. Extreme Weather Events by Crop Yield",
+    x = "Number of Extreme Weather Events",
+    y = "Economic Impact (Million USD)"
+  ) +
+  theme_minimal()
---
+++ Code/Temp_vs_Precip_Corr.R
@@ -0,0 +1,29 @@
+library(ggplot2)
+
+data <- read.csv("climate_change_impact_on_agriculture_2024.csv")  
+
+# calculate the correlations
+temp_yield_corr <- cor(data$Average_Temperature_C, data$Crop_Yield_MT_per_HA)
+precip_yield_corr <- cor(data$Total_Precipitation_mm, data$Crop_Yield_MT_per_HA)
+
+# Plot 1: Crop Yields vs Temperature
+ggplot(data, aes(x = Average_Temperature_C, y = Crop_Yield_MT_per_HA)) +
+  geom_point(color = "blue", alpha = 0.6) +  # Add points
+  labs(
+    title = "Crop Yields vs Temperature",
+    subtitle = paste("Correlation:", round(temp_yield_corr, 2)),
+    x = "Average Temperature (°C)",
+    y = "Crop Yield (Metric Tons per Hectare)"
+  ) +
+  theme_minimal() 
+
+# Plot 2:Crop Yields vs Precipitation
+ggplot(data, aes(x = Total_Precipitation_mm, y = Crop_Yield_MT_per_HA)) +
+  geom_point(color = "green", alpha = 0.6) +  
+  labs(
+    title = "Crop Yields vs Precipitation",
+    subtitle = paste("Correlation:", round(precip_yield_corr, 2)),
+    x = "Total Precipitation (mm)",
+    y = "Crop Yield (Metric Tons per Hectare)"
+  ) +
+  theme_minimal()  # theme
