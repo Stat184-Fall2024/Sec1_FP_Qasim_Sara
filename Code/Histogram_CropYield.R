708d1d096dd3de6462908c6ed11e85f7fc68090c
QasimA20 <qia5020@psu.edu>
2024-12-11 21:33:25 -0500

    Adding Crop Yield Histogram

---
+++ Code/Histogram_CropYield.R
@@ -0,0 +1,20 @@
+# Load necessary library
+library(ggplot2)
+data <- read.csv("climate_change_impact_on_agriculture_2024.csv")
+
+# Plot the histogram for crop yields
+ggplot(data, aes(x = Crop_Yield_MT_per_HA)) + 
+  geom_histogram( #Create histogram
+    binwidth = 0.5, 
+    fill = "steelblue", 
+    color = "black", 
+    alpha = 0.7
+  ) +
+  labs(
+    title = "Distribution of Crop Yields",
+    subtitle = "A closer look at the spread of crop yields across regions",
+    caption = "Source: Climate Change Impact on Agriculture Dataset",
+    x = "Crop Yield (Metric Tons per Hectare)",
+    y = "Frequency"
+  ) +
+  theme_minimal()
