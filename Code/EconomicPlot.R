d00dc1e1198f19cfc312e4558cbe4539737a925a
QasimA20 <qia5020@psu.edu>
2024-12-18 01:41:26 -0500

    Update EconomicPlot.R

--- Code/EconomicPlot.R
+++ Code/EconomicPlot.R
@@ -3,10 +3,10 @@ library(ggplot2)
 data_climate_impact <- read.csv("climate_change_impact_on_agriculture_2024.csv") 
 
 #Create the scatter plot
-ggplot(data_climate_impact, aes(x = Extreme_Weather_Events, 
+ggplot(data_climate_impact, aes(x = Extreme_Weather_Events, # names
                                 y = Economic_Impact_Million_USD, 
                                 color = Crop_Yield_MT_per_HA)) +
-  geom_point(alpha = 0.7, size = 3) +
+  geom_point(alpha = 0.7, size = 3) + #specifc graph aesthetics
   scale_color_viridis_c(name = "Crop Yield (MT/HA)") +
   labs(
     title = "Economic Impact vs. Extreme Weather Events by Crop Yield",
